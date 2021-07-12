3c0d2220022eed15ef87cd62fe5ec4b12568819e
ichiro sugimoto <ichiro.sugimoto@isee.nagoya-u.ac.jp>
2021-07-12 18:56:22 +0900

    [add]タイトルとページのリンク作成,ページに日時の追加

--- app/Post.php
+++ app/Post.php
@@ -8,6 +8,6 @@ class Post extends Model
 {
     public function getPaginateByLimit(int $limit_count = 10)
     {
-    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
-    }
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }  
 }
