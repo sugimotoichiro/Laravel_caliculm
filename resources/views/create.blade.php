b4d9252d333fe5da26ac9e32ee4dd29bb6fb50f2
ichiro sugimoto <ichiro.sugimoto@isee.nagoya-u.ac.jp>
2021-07-15 21:32:17 +0900

    [add]ブログ登校画面のバリデーション

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -18,11 +18,13 @@
                 @csrf
                 <div class ="title">
                     <h2>Title</h2>
-                     <input type="text" name="post[title]" placeholder="タイトル"/>
+                    <input type="text" name="post[title]" placeholder="タイトル" value="{{old("post.title")}}"/>
+                    <p class ="title_error" style="color:red">{{$errors->first("post.title")}}</p>
                 </div>
                 <div class ="body">
                     <h2>body</h2>
-                    <textarea name="post[body]" placeholder="今日もお疲れ様でした"><</textarea>
+                    <textarea name="post[body]" placeholder="今日もお疲れ様でした"> {{old("post.body")}}</textarea>
+                    <p class ="body_error" style="color:red">{{$errors->first("post.body")}}</p>
                 </div>
                 <input type="submit" value="store">
             </form>
